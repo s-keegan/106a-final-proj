1a582730f0a245830b1f2e11b9bb2f0c2d449a22
Ashlee Heuston <aheuston@berkeley.edu>
2017-12-08 13:39:04 -0800

    deleted old / junk files

--- commmandSawyer.py
+++
@@ -1,7 +0,0 @@
-import rospy
-import intera_io as io
-import intera_interface
-import intera_core_msgs
-rospy.init_node("test_commands")
-cmd = io.io_interface.IOInterface("/io/internal_camera/head_camera", intera_core_msgs.msg.IOComponentCommand, intera_core_msgs.msg.IODeviceStatus)
-print "cmd.publish_command('set_exposure', {})"
